82912756e517dd3959fcb1c925fec85a4acea300
Kira <kartoshka0111maona@email.com>
2026-01-04 23:03:47 +0200

    pack new

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -25,14 +25,15 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBBhlSSOUVK19XPEiMB3IbifjRiOoxEGQo",
-  authDomain: "brandpaint-784a0.firebaseapp.com",
-  projectId: "brandpaint-784a0",
-  storageBucket: "brandpaint-784a0.firebasestorage.app",
-  messagingSenderId: "95796336272",
-  appId: "1:95796336272:web:5b14f62bf621e6f015aa15",
-  measurementId: "G-Q86CH40JVT"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
+
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
